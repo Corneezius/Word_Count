370fd41e227f424d8b9a3adb9fc60690f362b1f1
cfr2116@columbia.edu <Guest@Epicoduss-iMac-55.local>
2016-09-22 01:14:45 -0400

    Add tests

--- test/WordTest.php
+++ test/WordTest.php
@@ -1,16 +1,18 @@
 <?php
-    require_once "src/word.php";
 
-    class WordTest extends PHPUnit_Framework_TestCase{
+    require_once __DIR__."/../src/word.php";
+
+    class Word extends PHPUnit_Framework_TestCase{
 
 
         function test_OneMatch(){
 
           //Arrange
+
           $test_sentence = "Ducks are delicious.";
           $test_palabra= "Ducks";
           //Act
-          $result = $test_Word->word_count($test_sentence, $test_palabra);
+          $result = $this->word_count($test_sentence, $test_palabra);
           //Assert
           $this->assertEquals("1", $result);
         }
@@ -21,20 +23,20 @@
           $test_sentence = "Ducks are delicious and Ducks are Ducks.";
           $test_palabra= "Ducks";
             //Act
-          $result = $test_Word->word_count($test_sentence, $test_palabra);
+          $result = $this->word_count($test_sentence, $test_palabra);
             //Assert
           $this->assertEquals("3", $result);
         }
 
-        function test_MultipleMatch()
+        function test_NoMatch()
         {
             //Arrange
           $test_sentence = "Chickens are bettern that other Ucks.";
           $test_palabra= "Ducks";
             //Act
-          $result = $test_Word->word_count($test_sentence, $test_palabra);
+          $result = $this->word_count($test_sentence, $test_palabra);
             //Assert
           $this->assertEquals("0", $result);
         }
-    ]
+    }
 ;
